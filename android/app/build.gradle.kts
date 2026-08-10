b6bc75b5708b8e2bf9c0a8864d6eb896d0142f7e
karlos1998 <fadeusz98@gmail.com>
2026-08-10 11:07:05 +0200

    chore(release): bump history feature versions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "it.letscode.tougedash"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.6.0"
+        versionCode = 13
+        versionName = "1.7.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
         buildConfigField("String", "API_BASE_URL", "\"https://touge-dash-engine.letscode.it\"")
