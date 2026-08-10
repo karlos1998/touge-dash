4c9130c919fb04a5368e843fde9d401d0e637080
karlos1998 <fadeusz98@gmail.com>
2026-08-10 23:07:05 +0200

    Bump mobile app versions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "it.letscode.tougedash"
         minSdk = 26
         targetSdk = 35
-        versionCode = 17
-        versionName = "1.8.3"
+        versionCode = 18
+        versionName = "1.8.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
         buildConfigField("String", "API_BASE_URL", "\"https://touge-dash-engine.letscode.it\"")
